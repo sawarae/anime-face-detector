7734f188bfc4590374fa9d31b06e292310509d75
sawarae <sawaraengine@gmail.com>
2026-01-17 16:48:44 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='anime-face-detector',
+    version='0.1.0',
+    description='Anime Face Detector using mmdet and mmpose',
+    readme='README.md',
+    license='MIT',
+    author='hysts',
+    python_requires='>=3.10,<3.12',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.3,<2.0',
+        'opencv-python-headless>=4.5.4.58',
+        'torch>=2.7.0',
+        'torchvision>=0.18.0',
+    ],
+    extras_require={
+        'demo': [
+            'gradio>=4.0.0',
+        ],
+        'openmmlab': [
+            'mmengine>=0.7.0',
+            'mmcv>=2.1.0',
+            'mmdet>=3.2.0',
+            'mmpose>=1.3.2',
+        ],
+    },
+    url='https://github.com/hysts/anime-face-detector',
+)
